a80885698b7033960e8c2c018c35112be033896a
黃翔 <146065123+sean112310234@users.noreply.github.com>
2023-11-06 00:20:03 -0600

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main()
+{
+  int main;
+  int hour,min;
+  double ha,ma,tmp1,tmp2,angle;
+  scanf("%d %d",&hour,&min);
+  ha=(hour*60+min)*0.5;
+  ma=min*6;
+  if(ha<=ma){
+      tmp1=ma;
+      tmp2=ha;
+  }
+  else{
+      tmp1=ha;
+      tmp2=ma;
+  }
+  angle=tmp1-tmp2;
+  printf("%0.3f\n",angle);
+}
