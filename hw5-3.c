1ffb1777936e869e3f8d8a46d4b70102d05aa215
黃翔 <146065123+sean112310234@users.noreply.github.com>
2023-11-06 00:29:01 -0600

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -10,8 +10,8 @@ int main()
     t1=tim*rate;
     printf("%0.1f\n",t1);
   }
-  else if(tim>60&&tim<=120){
-    t2= 60*rate+(tim-60)*rate*1.3;
+  else if(tim>60 && tim<=120){
+    t2= 60*rate+(tim-60)*rate*1.33;
     printf("%0.1f\n",t2);
   }
   else if(tim>120){
