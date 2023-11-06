c3b6356626bbdbd4486c65396b7c4d3ec3ce4527
黃翔 <146065123+sean112310234@users.noreply.github.com>
2023-11-06 00:04:03 -0600

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -4,19 +4,19 @@ int main()
 {
   int main;
   int tim,rate;
-  double total;
+  double t1,t2,t3;
   scanf("%d %d",&tim,&rate);
   if(tim<=60){
-    total=tim*rate;
-    printf("%0.1f\n",total);
+    t1=tim*rate;
+    printf("%0.1f\n",t1);
   }
   else if(tim>60&&tim<=120){
-    total=tim*rate*1.3;
-    printf("%0.1f\n",total);
+    t2= t1+(tim-60)*rate*1.3;
+    printf("%0.1f\n",t2);
   }
   else if(tim>120){
-    total=tim*rate*1.66;
-    printf("%0.1f\n",total);
+    t3=t2+(tim-120)*rate*1.66;
+    printf("%0.1f\n",t3);
   }
 
 }
