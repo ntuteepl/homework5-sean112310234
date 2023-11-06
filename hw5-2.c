ac8feb1c748a6c945182a1769a82830d1cd658c4
黃翔 <146065123+sean112310234@users.noreply.github.com>
2023-11-05 23:46:34 -0600

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -8,15 +8,15 @@ int main()
   scanf("%d",&min);
   if(min<=800){
     f=min*0.9;
-    printf("%0.2f\n",f);
+    printf("%0.1f\n",f);
   }
   else if(min>800&&min<=1500){
     f=min*0.9*0.9;
-    printf("%0.2f\n",f);
+    printf("%0.1f\n",f);
   }
   else if(min>1500){
     f=min*0.79*0.9;
-    printf("%0.2f\n",f);
+    printf("%0.1f\n",f);
   }
 
 }
